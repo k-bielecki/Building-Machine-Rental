afe55402e16f86bbd06c40f3f335007a2b666efe
Krzysztof <krzysztof.b.bielecki@gmail.com>
2024-06-20 23:23:00 +0200

    adding Dtos to rent controller

--- src/main/java/com/myshop/rent/infrastructure/api/RentController.java
+++ src/main/java/com/myshop/rent/infrastructure/api/RentController.java
@@ -1,6 +1,8 @@
 package com.myshop.rent.infrastructure.api;
 
 import com.myshop.machine.domain.Machine;
+import com.myshop.machine.infrastructure.api.MachineDetailsDto;
+import com.myshop.machine.infrastructure.api.MachineDto;
 import com.myshop.rent.domain.InvalidRentException;
 import com.myshop.rent.domain.Rent;
 import com.myshop.rent.domain.RentFacade;
@@ -25,23 +27,53 @@ public class RentController {
     }
 
     @GetMapping("/rents")
-    public List<Rent> getAllRents() {
-        return rentFacade.getAllRents();
+    public List<RentDto> getAllRents() {
+        return rentFacade.getAllRents().stream()
+                .map(rent -> toDto(rent))
+                .toList();
     }
 
     @GetMapping("/rents/{userId}")
-    public List<Rent> getAllRentsByUserId(@PathVariable Long userId) {
-        return rentFacade.getAllRentsByUser(userId);
+    public List<RentDto> getAllRentsByUserId(@PathVariable Long userId) {
+        return rentFacade.getAllRentsByUser(userId).stream()
+                .map(rent -> toDto(rent))
+                .toList();
     }
 
     @GetMapping("/rents/machines")
-    public List<Machine> getAllRentedMachines() {
-        return rentFacade.getAllRentedMachines();
+    public List<MachineDto> getAllRentedMachines() {
+        return rentFacade.getAllRentedMachines().stream()
+                .map(machine -> MachineDto.builder()
+                        .name(machine.getName())
+                        .pricePerDay(machine.getPricePerDay())
+                        .rented(machine.getRented())
+                        .condition(machine.getCondition())
+                        .detailsDto(MachineDetailsDto.builder()
+                                .machineHour(machine.getDetails().getMachineHour())
+                                .weight(machine.getDetails().getWeight())
+                                .horsePower(machine.getDetails().getHorsePower())
+                                .category(machine.getDetails().getCategory())
+                                .build())
+                        .build())
+                .toList();
     }
 
     @GetMapping("/rents/machines/{userId}")
-    public List<Machine> getAllRentedMachinesByUser(@PathVariable Long userId) {
-        return rentFacade.getAllRentedMachinesByUser(userId);
+    public List<MachineDto> getAllRentedMachinesByUser(@PathVariable Long userId) {
+        return rentFacade.getAllRentedMachinesByUser(userId).stream()
+                .map(machine -> MachineDto.builder()
+                        .name(machine.getName())
+                        .pricePerDay(machine.getPricePerDay())
+                        .rented(machine.getRented())
+                        .condition(machine.getCondition())
+                        .detailsDto(MachineDetailsDto.builder()
+                                .machineHour(machine.getDetails().getMachineHour())
+                                .weight(machine.getDetails().getWeight())
+                                .horsePower(machine.getDetails().getHorsePower())
+                                .category(machine.getDetails().getCategory())
+                                .build())
+                        .build())
+                .toList();
     }
 
     @PostMapping("/rent")
@@ -62,6 +94,10 @@ public class RentController {
             return ResponseEntity.badRequest().body(exception.getErrors());
         }
         return ResponseEntity.ok().build();
+    }
+
+    private RentDto toDto(Rent rent) {
+        return new RentDto(rent.getUserId(), rent.getMachineId());
     }
 
 
